776cd0a3dad8085adb545ae9a7988cf8641f0093
syankel <s.shvirtz@gmaul.com>
2025-11-29 20:20:47 +0200

    Storing the quentity of each number in an array in arr monim

--- src/PartTwo.java
+++ src/PartTwo.java
@@ -1,6 +1,9 @@
 public class PartTwo {
 
     public static void strictlyIncreasing(int[] arr) {
+        int[] arr_monim = new int[10];
+
+        for (int i = 0; i < arr.length; arr_monim[arr[i]]++, i++);
         
     }
 
